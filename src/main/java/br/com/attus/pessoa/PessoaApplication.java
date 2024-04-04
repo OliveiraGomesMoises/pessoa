11cbbdb359d8ceffc7a51e7a65c9e8909641a270
OliveiraGomesMoises <mosesgdo@gmail.com>
2024-04-03 21:46:14 -0300

    implementa api de teste

--- src/main/java/br/com/attus/pessoa/PessoaApplication.java
+++ src/main/java/br/com/attus/pessoa/PessoaApplication.java
@@ -2,10 +2,18 @@ package br.com.attus.pessoa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/") 
 public class PessoaApplication {
-
+	@GetMapping
+	public String getHomeTeste() {
+		return "PESSOA - API HOME";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(PessoaApplication.class, args);
 	}
