58351e49887c24918b93e9cade4fc6a50e7949b0
OliveiraGomesMoises <mosesgdo@gmail.com>
2024-04-05 14:38:42 -0300

    implementa request

--- src/main/java/br/com/attus/pessoa/application/api/PessoaRequest.java
+++ src/main/java/br/com/attus/pessoa/application/api/PessoaRequest.java
@@ -1,5 +1,19 @@
 package br.com.attus.pessoa.application.api;
 
+import java.time.LocalDate;
+
+import org.springframework.lang.NonNull;
+
+import br.com.attus.pessoa.domain.Endereco;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Value;
+
+@Value
 public class PessoaRequest {
+	@NotBlank
+	private String nomeCompleto;
+	@NonNull
+	private LocalDate dataDeNascimento;
+	private Endereco endereco;
 
 }
