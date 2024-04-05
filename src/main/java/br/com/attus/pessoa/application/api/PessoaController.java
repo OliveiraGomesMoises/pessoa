b318f9cb8117505bc8661ca2618b2fb1524486c4
OliveiraGomesMoises <mosesgdo@gmail.com>
2024-04-05 14:18:15 -0300

    implementa controller e fluxo para service

--- src/main/java/br/com/attus/pessoa/application/api/PessoaController.java
+++ src/main/java/br/com/attus/pessoa/application/api/PessoaController.java
@@ -2,17 +2,22 @@ package br.com.attus.pessoa.application.api;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.attus.pessoa.application.service.PessoaService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @RestController
 @Log4j2
+@RequiredArgsConstructor
 public class PessoaController implements PessoaAPI {
+	private final PessoaService pessoaService;
 
 	@Override
 	public PessoaResponse postPessoa(PessoaRequest pessoarequest) {
 		log.info("[inicia] PessoaController - postPessoa");
+		PessoaResponse pessoaCriado = pessoaService.criaPessoa(pessoarequest);
 		log.info("[finaliza] PessoaController - postPessoa");
-		return null;
+		return pessoaCriado;
 	}
 
 }
---
+++ src/main/java/br/com/attus/pessoa/application/service/PessoaService.java
@@ -0,0 +1,9 @@
+package br.com.attus.pessoa.application.service;
+
+import br.com.attus.pessoa.application.api.PessoaRequest;
+import br.com.attus.pessoa.application.api.PessoaResponse;
+
+public interface PessoaService {
+	PessoaResponse criaPessoa(PessoaRequest pessoarequest);
+
+}
